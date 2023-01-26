aa995b567d4e165f9da308e5837342dd011d6ef7
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2023-01-26 11:38:29 +0000

    oof

---
+++ 1st try.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+# connect to the database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="rfid",
+  password="ad"
+  data
+)
+
+print(mydb)
+
+x=1 
+
+while x == 1:
+    print ("1")
