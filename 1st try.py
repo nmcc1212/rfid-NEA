d627b2f2af4d4ee86963e30932556b7870d28e62
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2023-01-28 23:24:13 +0000

    fix functions

--- 1st try.py
+++ 1st try.py
@@ -1,45 +1,71 @@
 import mysql.connector
-import datetime
 import RPi.GPIO as GPIO
+from datetime import datetime
 
-def dbinit():
+# Connect to MySQL database
+def connect_to_database():
     mydb = mysql.connector.connect(
-        host="hostname",
-        user="rfid2",
-        password="ad",
-        database="rfid-project"
+      host="localhost",
+      user="rfid2",
+      password="ad",
+      database="rfid2"
     )
-    mycursor = mydb.cursor()
+    return mydb
 
-def readcard():
-    uid = ""
+# Set up RFID reader and read card ID
+def read_card_id():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.IN)
+
+    card = ""
     while True:
         if GPIO.input(7) == 1:
-            uid = input("Scan RFID card: ")
-            print (uid)
+            card = input("Scan RFID card: ")
             break
-    return uid
-def gettime():
-    now = datetime.now()
-    time_stamp = now.strftime("%Y-%m-%d %H:%M:%S")
-    return time_stamp
-	
-def	GetStatusState(): 
-    sql = "SELECT binary_value FROM rfid_cards WHERE uid = %s"
-    val = (uid,)
+    return card
+
+# Retrieve current binary value from database
+def get_current_value(mydb, card):
+    mycursor = mydb.cursor()
+    sql = "SELECT binary_value FROM card_states WHERE card_id = %s"
+    val = (card,)
     mycursor.execute(sql, val)
     result = mycursor.fetchone()
-    return result
+    return result[0]
 
-def statusflip():
-    if result[0] == 0:
+# Flip binary value
+def flip_value(value):
+    if value == 0:
         new_value = 1
     else:
         new_value = 0
     return new_value
 
-def insertflip():
-    sql = "UPDATE rfid_cards SET binary_value = %s WHERE uid = %s"
-    val = (new_value, uid)
+# Get the current date and time
+def get_time_stamp():
+    now = datetime.now()
+    time_stamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    return time_stamp
+
+# Update database with new value and timestamp
+def update_database(mydb, card, new_value, time_stamp):
+    mycursor = mydb.cursor()
+    sql = "UPDATE card_states SET binary_value = %s, timestamp = %s WHERE card_id = %s"
+    val = (new_value, time_stamp, card)
     mycursor.execute(sql, val)
     mydb.commit()
+
+# Main function
+def main():
+    mydb = connect_to_database()
+    card = read_card_id()
+    current_value = get_current_value(mydb, card)
+    new_value = flip_value(current_value)
+    time_stamp = get_time_stamp()
+    update_database(mydb, card, new_value, time_stamp)
+    print("Card ID:", card)
+    print("Binary value updated to:", new_value)
+    print("Time Stamp:", time_stamp)
+
+
+main()
---
+++ sqlcode.sql
@@ -0,0 +1,11 @@
+/* Iteration 1*/
+
+CREATE TABLE card_states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    card_id VARCHAR(255) NOT NULL,
+    binary_value TINYINT(1) NOT NULL,
+    timestamp DATETIME NOT NULL
+);
+
+
+/* Iteration 2*/
