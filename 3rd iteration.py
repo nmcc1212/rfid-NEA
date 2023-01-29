a4f5f9a984b8295bb3edbef5939ca709619f45fc
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2023-01-29 01:52:30 +0000

    new ui

--- 3rd iteration.py
+++ 3rd iteration.py
@@ -1,41 +1,38 @@
 from flask import Flask, render_template
 import mysql.connector
-import RPi.GPIO as GPIO
-from datetime import datetime
-from mfrc522 import SimpleMFRC522
-import atexit
-
-atexit.register(GPIO.cleanup)
 
 app = Flask(__name__)
 
-@app.route("/")
-def get_card_status():
+def connect_to_database():
     mydb = mysql.connector.connect(
-      host="localhost",
-      user="rfid2",
-      password="ad",
-      database="rfid2"
+        host="localhost",
+        user="rfid2",
+        password="ad",
+        database="rfid2"
     )
+    return mydb
+
+def get_all_cards():
+    mydb = connect_to_database()
     mycursor = mydb.cursor()
+    sql = "SELECT card_id, binary_value, timestamp, name FROM card_states"
+    mycursor.execute(sql)
+    result = mycursor.fetchall()
+    cards = []
+    for card in result:
+        cards.append({
+            'card_id': card[0],
+            'binary_value': card[1],
+            'timestamp': card[2],
+            'name': card[3]
+        })  
+    return cards
 
-    reader = SimpleMFRC522()
-    card = ""
-    while True:
-        card, text = reader.read()
-        break
+@app.route('/')
+def index():
+    cards = get_all_cards()
+    return render_template('index.html', cards=cards)
 
-    sql = "SELECT binary_value FROM card_states WHERE card_id = %s"
-    val = (card,)
-    mycursor.execute(sql, val)
-    result = mycursor.fetchone()
-
-    if result:
-        current_value = result[0]
-    else:
-        current_value = None
-
-    return render_template("3rd-iteration.html", current_value=current_value, card_id=card)
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
+``
